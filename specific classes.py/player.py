2f9b7ef807339d2380eb2ebc87226dda9303f3aa
carav3 <carav3@nycstudents.net>
2023-11-28 13:59:05 -0500

    hi

---
+++ specific classes.py/player.py
@@ -0,0 +1,6 @@
+class player:
+    def __init__(self, name, inventory):
+        self.name = name
+        self.inventory = inventory
+    def pickup(self, item):
+        self.inventory.append(item)
