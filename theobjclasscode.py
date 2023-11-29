94456c8afc701b15cc4a37ae701fff7b16b17e8f
STellam73 <144480837+STellam73@users.noreply.github.com>
2023-11-29 13:59:26 -0500

    the objectclasses.py

---
+++ theobjclasscode.py
@@ -0,0 +1,9 @@
+# vase, briefcase,book,spatula, frog,crown,tophat,necklace,umbrella
+class obj():
+    def __init__(self,color, size, charecteristic):
+        self.color = color
+        self.size = size
+        self.charceteristic = charecteristic
+
+Vase = obj("Vase", "blue", "medium size","floral design")
+Briefcase = obj("Briefcase","black","small size","leather material")
