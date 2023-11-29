3f7ec4ea7821fe83ad7ba3d2075f42f1dafed12f
carav3 <carav3@nycstudents.net>
2023-11-29 13:58:08 -0500

    Update whalen.py

--- whalen.py
+++ whalen.py
@@ -12,12 +12,28 @@ class Room_one(Room):
                 if pickup == "yes":
                         print("Congratulations, you have won the game.")
 
-item = "Frog"
-room_one = Room_one(1, ["Frog", "Comb"])
+item = "frog"
+room_one = Room_one(1, ["frog", "spatula", "book"]) # replace spatula in second hallway room one
 room_one.message(item)
 
 class Room_two(Room):
         def __init__self(self, number, items):
                 super().__init__(number, items)
         def message(self, item):
-                print(f"")
+                print(f"You hesitantly open the door, to find a master bedroom. With a few minutes of searching you find an {item}")
+                pickup = input("Do you want to pick the item up, if you believe it is the required item?")
+                if pickup == "yes":
+                        print("Congratulations, you have won the game.")
+item = "necklace"
+room_two = Room_two(2, ["necklace", "briefcase", "crown"])
+
+class Room_three(Room):
+        def __init__self(self, number, items):
+                super().__init__(number, items)
+        def message(self, item):
+                print(f"This is obviously the bathroom, and an elaborate one at that. Who needs four showers? You find {item}")
+                pickup = input("Do you want to pick the item up, if you believe it is the required item?")
+                if pickup == "yes":
+                        print("Congratulations, you have won the game.")
+item = "tophat"
+room_three = Room_three(3, ["tophat", "vase", "unbrella"])
