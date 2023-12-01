7a84dec5aab024a5d65fdf5b37b459141d60cb76
carav3 <carav3@nycstudents.net>
2023-12-01 13:30:52 -0500

    Update room class.py

--- room class.py
+++ room class.py
@@ -1,11 +1,12 @@
 
 class Room():
-        def __init__self(self, number, items):
+        def __init__self(self, number, hallway):
                 self.number = number
-                self.items = items
+                self.hallway = hallway
 class Room_one(Room):
-        def __init__self(self, hallway, items):
-                super().__init__(hallway, items)
+        def __init__self(self, item):
+                self.item = item
+                super().__init__(hallway)
         def message(self, item):  
                 print(f"You search through the room, quickly realizing it is a kitchen. Somehow, you find a {item}")
 
@@ -16,7 +17,7 @@ hallway = 1
 #room_one = Room_one(1, ["frog", "spatula", "book"]) # replace spatula in second hallway room one
 #room_one.message(item, hallway)
 
-room_one_one = Room(1, ["frog"])
+room_one_one = Room_one(["frog"])
 print(room_one_one.number)
 
 class Room_two(Room):
