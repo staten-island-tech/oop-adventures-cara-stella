60eb2870a492001f83ac058a774a77b7740c2fbd
carav3 <carav3@nycstudents.net>
2023-12-01 13:58:06 -0500

    update

--- room class.py
+++ room class.py
@@ -17,36 +17,46 @@ class Room_one(Room):
 #room_one.message(item, hallway)
 
 room_one_one = Room_one(1, ["frog"])
-print(room_one_one.item)
+#print(room_one_one.item)
+room_two_one = Room_one(1, ["spatula"])
+room_three_one = Room_one(1, ["book"])
 
 class Room_two(Room):
-        def __init__self(self, hallway, items):
-                super().__init__(hallway, items)
+        def __init__self(self, hallway, item):
+                super().__init__(hallway)
+                self.item = item
         def message(self, item):
                 print(f"You hesitantly open the door, to find a master bedroom. With a few minutes of searching you find an {item}")
                 pickup = input("Do you want to pick the item up, if you believe it is the required item?")
                 if pickup == "yes":
                         print("Congratulations, you have won the game.")
                         
-item = "necklace"
-hallway = 1
-room_two = Room_two(2, [1, 2, 3], ["necklace", "briefcase", "crown"])
-room_two.message(item, hallway)
+#item = "necklace"
+#hallway = 1
+#room_two = Room_two(2, [1, 2, 3], ["necklace", "briefcase", "crown"])
+#room_two.message(item, hallway)
+
+room_one_two = Room_two(2, ["necklace"])
+room_two_two = Room_two(2, ["briefcase"])
+room_three_two = Room_two(2, ["crown"])
 
 class Room_three(Room):
-        def __init__self(self, hallway, items):
-                super().__init__(hallway, items)
+        def __init__self(self, hallway, item):
+                super().__init__(hallway)
+                self.item = item
         def message(self, item):
                 print(f"This is obviously the bathroom, and an elaborate one at that. You find {item}")
                 pickup = input("Do you want to pick the item up, if you believe it is the required item?")
                 if pickup == "yes":
                         print("Congratulations, you have won the game.")
-item = "tophat"
-hallway = 1
-room_three = Room_three(3, [1, 2, 3], ["tophat", "vase", "unbrella"])
-room_two.message(item, hallway)
+#item = "tophat"
+#hallway = 1
+#room_three = Room_three(3, [1, 2, 3], ["tophat", "vase", "unbrella"])
+#room_two.message(item, hallway)
 
-hallway = [Room_one(), Room_two(), Room_three]
-print(hallway)
+room_one_three = Room_three(3, ["tophat"])
+room_two_three = Room_three(3, ["vase"])
+room_three_three = Room_three(3, ["umbrella"])
+
 
 
