f68c709dff55ca2cc782700636b5d8bff64bbd36
carav3 <carav3@nycstudents.net>
2023-12-07 13:52:34 -0500

    Update room class.py

--- room class.py
+++ room class.py
@@ -12,17 +12,18 @@ class Room_one(Room):
                 self.item = item
         def message(self):
                 print(f"You search through the room, quickly realizing it is a kitchen. Somehow, you find a {self.item}")
-
+                pick_up()
+                
 
 #item = "frog"
 #hallway = 1
 #room_one = Room_one(1, ["frog", "spatula", "book"]) # replace spatula in second hallway room one
 #room_one.message(item, hallway)
-room_one_one = Room_one(1, item = ["frog"]) # i added the item = but lets see if it works
+room_one_one = Room_one(1, item = "frog") # i added the item = but lets see if it works
 
-room_two_one = Room_one(1, item = ["spatula"])
+room_two_one = Room_one(1, item = "spatula")
 #print(room_two_one.item)
-room_three_one = Room_one(1, item = ["book"])
+room_three_one = Room_one(1, item = "book")
 
 class Room_two(Room):
         def __init__(self, hallway, item):
@@ -33,16 +34,18 @@ class Room_two(Room):
                 #pickup = input("Do you want to pick the item up, if you believe it is the required item?")
                 #if pickup == "yes":
                 #        print("Congratulations, you have won the game.")
+                pick_up()
+            
                         
 #item = "necklace"
 #hallway = 1
 #room_two = Room_two(2, [1, 2, 3], ["necklace", "briefcase", "crown"])
 #room_two.message(item, hallway)
 
-room_one_two = Room_two(2, 'necklace')
+room_one_two = Room_two(2, item = 'necklace')
 #room_one_two.message()
-room_two_two = Room_two(2, "briefcase")
-room_three_two = Room_two(2, "crown")
+room_two_two = Room_two(2, item = "briefcase")
+room_three_two = Room_two(2, item = "crown")
 
 class Room_three(Room):
         def __init__(self, hallway, item):
@@ -50,23 +53,24 @@ class Room_three(Room):
                 self.item = item
         def message(self):
                 print(f"This is obviously the bathroom, and an elaborate one at that. You find {self.item}")
-                pickup = input("Do you want to pick the item up, if you believe it is the required item?")
-                if pickup == "yes":
-                        print("Congratulations, you have won the game.")
+                pick_up()
+                #pickup = input("Do you want to pick the item up, if you believe it is the required item? this will repeat, please repeat answer")
+                #if pickup == "yes":
+                        #print("Congratulations, you have won the game.")
 #item = "tophat"
 #hallway = 1
 #room_three = Room_three(3, [1, 2, 3], ["tophat", "vase", "unbrella"])
 #room_two.message(item, hallway)
 
-room_one_three = Room_three(3, ["tophat"])
-room_two_three = Room_three(3, ["vase"])
-room_three_three = Room_three(3, ["umbrella"])
+room_one_three = Room_three(3, item = 'tophat')
+room_two_three = Room_three(3, item = "vase")
+room_three_three = Room_three(3, item = "umbrella")
 
 
 item = []
 
 def pick_up():
-        pick_up = input(print("If you believe this is the required item, will you pick it up?"))
+        pick_up = input(("If you believe this is the required item, will you pick it up?"))
         if pick_up == "yes":
                 item.append
                 print (item)
@@ -75,9 +79,9 @@ def pick_up():
                 if item != obj:
                         print("You have lost. The game has ended.")
         if pick_up == "no":
-                hchoice = input(print("Choose a different hallway to travel down."))
+                hchoice = input(("Choose a different hallway to travel down."))
                 if hchoice == "one":
-                        rchoice = input(print("Choose a room - one, two, or three. Remember you may have already travelled inside some."))
+                        rchoice = input(("Choose a room - one, two, or three. Remember you may have already travelled inside some."))
                         if rchoice == "one":
                                 room_one_one.message()
                                 pick_up()
@@ -87,7 +91,7 @@ def pick_up():
                         if rchoice == "three":
                                 room_three_one.message()
                                 pick_up()
-                elif hchoice == "two":
+                if hchoice == "two":
                         if rchoice == "one":
                                 room_one_two.message()
                                 pick_up()
@@ -97,7 +101,7 @@ def pick_up():
                         if rchoice == "three":
                                 room_three_two.message()
                                 pick_up()
-                elif hchoice == "three":
+                if hchoice == "three":
                         if rchoice == "one":
                                 room_one_three.message()
                                 pick_up()
@@ -107,8 +111,8 @@ def pick_up():
                         if rchoice == "three":
                                 room_three_three.message()
                                 pick_up()
-
-print(room_three_three.message)
+#print("King Michael stares you down, and you decide to find the item in order to return your grandma to her nursing home.")
+#room_three_three.message()
 pick_up()
 
                 
