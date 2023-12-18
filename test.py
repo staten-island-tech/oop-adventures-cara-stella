cf5ce76d612f45c8a9ac8196279ad4567eb103a7
carav3 <carav3@nycstudents.net>
2023-12-18 13:55:22 -0500

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import random
 from roomclass import *
 
 # testing to see if the new code works here, not actual game
-print("You were strolling on a walk with your grandmother when suddenly a rainbow van with frog blocks your path.")
+print("You were strolling on a walk with your grandmother when suddenly a rainbow van with frogs blocks your path.")
 print("You blink for one second, and suddenly, your grandmother is gone.")
 print("You begin to panic frantically, and try to remember a minor detail from the van.")
 print("The one thing you were able to recall was the name printed on the van: Evil King M the third")
@@ -10,7 +10,7 @@ print("Your enter the name into your phone and recieved coordinates of the mansi
 print("You type the coordinates into google maps and follow the directions")
 print("Suspiciously and carefully, you stumble into the mansion in the middle of an enigmatic forest, desperate to return your grandma to her retirement home")
 print("Suddenly, a mysterious figure appears from the shadows.")
-a = input("'Hello, my name is King M the Third. Would you like to play a game young one? If you win, you get your grandma back.'") #press enter 
+a = input("'Hello, my name is King M the Third. Would you like to play a game young one? If you win, you get your grandma back.'")
 if a == "yes":
     import random
     obj = random.choice(['vase','book','frog', 'umbrella','spatula', ' briefcase', 'tophat', 'necklace', 'crown' ])
