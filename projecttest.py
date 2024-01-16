ae1866e35ae8eba7c41df8be2a91053f839cc58a
STellam73 <144480837+STellam73@users.noreply.github.com>
2024-01-16 13:29:32 -0500

    hjghfc

---
+++ projecttest.py
@@ -0,0 +1,85 @@
+import random 
+
+obj = random.choice(['vase','book','frog', 'umbrella','spatula', ' briefcase', 'top hat', 'necklace', 'crown' ])
+print(f"You are required to find {obj} within the house. Good luck.")
+
+vase = "vase"
+book = "book"
+frog = "frog"
+umbrella = "umbrella"
+spatula = "spatula"
+briefcase = 'briefcase'
+tophat = "tophat"
+necklace = "necklace"
+crown = "crown"
+
+
+firsthallway = input("There are two hallways to walk down.  Which do you choose?")
+
+if firsthallway == "hallway one":
+    print("As you walk down the eerie hallway, you realize that you can enter three rooms.")
+    firstroom = input("Which do you choose to enter first?")
+    if firstroom == "room one":
+        print(f"You search through the room, quickly realizing it is a kitchen. Somehow, you find a {frog}")
+        pickup = input("Do you want to pick the item up, if you believe it is the required item?")
+        if pickup == "yes":
+            if obj == "frog":
+                print("Congratulations, you have won the game.")
+                quit()
+    if firstroom == "room two":
+        print(f"This room appears to be a master bedroom, unoccupied for a while now. After searching, you find an old {necklace}")
+        pickup = input("Do you want to pick the item up, if you believe it is the required item?")
+        if pickup == "yes":
+            if obj == "necklace":
+                print("Congratulations, you have won the game.")
+                quit()
+    if firstroom == "room three":
+        print(f"This room is clearly the living room. You find a {tophat}")
+        pickup = input("Do you want to pick the item up, if you believe it is the required item?")
+        if pickup == "yes":
+            if obj == "tophat":
+                print("Congratulations, you have won the game.")
+                quit()
+if firsthallway == "hallway two":
+    print("As you walk down the colorful hallway that reminds you of candyland, you realize you can enter three rooms.")
+    firstroom = input("Which do you choose to enter first?")
+    if firstroom == "room one":
+        print(f"This is the bathroom. You somehow find a {crown} belonging to King Michael Whalen the First.")
+        pickup = input("Do you want to pick the item up, if you believe it is the required item?")
+        if pickup == "yes":
+            if obj == "crown":
+                print("Congratulations, you have won the game.")
+                quit()
+            elif obj != "crown" or pickup == "no":
+                print("Proceed to next chosen room.")
+                secondroom = input("Which room do you want to try, excluding the first room?")
+                if secondroom == "room two":
+                    print(f"The door creaks open, making a dusty wooden desk visible. Opening the drawer, you find a {book}")
+                    pickup = input("Do you want to pick the item up, if you believe it is the required item?")
+                    if pickup == "yes":
+                        if obj == "book":
+                            print("Congratulations, you have won the game.")
+                            quit()
+    if firstroom == "room two":
+        print(f"The door creaks open, making a dusty wooden desk visible. Opening the drawer, you find a {book}")
+        pickup = input("Do you want to pick the item up, if you believe it is the required item?")
+        if pickup == "yes":
+            if obj == "book":
+                print("Congratulations, you have won the game.")
+                quit()
+    if firstroom == "room three":
+        print(f"The next room is the guest bedroom, and you find a fragile {vase}")
+
+        
+        pickup = input("Do you want to pick the item up, if you believe it is the required item?")
+        if pickup == "yes":
+            if obj == "vase":
+                print("Congratulations, you have won the game.")
+                quit()
+
+
+
+
+
+
+
---
+++ test.py
@@ -0,0 +1,44 @@
+
+
+
+
+
+
+
+
+
+
+# i think the reason the code may not be working is because when we are importing the Room class and the pickup function the words are grey and undefined. Idk why though?
+
+import random
+from roomclass import Room
+from roomclass import Room_one
+from roomclass import toNum
+from roomclass import pick_up
+# testing to see if the new code works here, not actual game
+print("You were strolling on a walk with your grandmother when suddenly a rainbow van with frog blocks your path.")
+print("You blink for one second, and suddenly, your grandmother is gone.")
+print("You begin to panic frantically, and try to remember a minor detail from the van.")
+print("The one thing you were able to recall was the name printed on the van: Evil King M the third")
+print("Your enter the name into your phone and recieved coordinates of the mansion")
+print("You type the coordinates into google maps and follow the directions")
+print("Suspiciously and carefully, you stumble into the mansion in the middle of an enigmatic forest, desperate to return your grandma to her retirement home")
+print("Suddenly, a mysterious figure appears from the shadows.")
+a = input("Hello, my name is King M the Third. Would you like to play a game young one? If you win, you get your grandma back.") #press enter 
+if a == "yes":
+    import random
+    obj = random.choice(['vase','book','frog', 'umbrella','spatula', ' briefcase', 'tophat', 'necklace', 'crown' ])
+    print(f"You are required to find {obj} within the house. Good luck.")
+
+    n = toNum(input("what hallway do you want to travel down?"))
+    if n == 1: #type out "one"
+        print(Room_one)
+
+        
+#if a == "no":
+ #   print("byebye grandma :)")
+  #  exit()
+
+
+
+
